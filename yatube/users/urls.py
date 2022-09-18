6fe096c83883ec029b48a4c249d16d8c493da429
Полина Николаева <sti-xa@ya.ru>
2022-09-18 18:32:54 +0300

    fixed bugs

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
         'password_reset/',
         PasswordResetView.as_view(
         template_name='users/password_reset_form.html',
-        success_url = reverse_lazy('users:password_reset_done')
+        success_url=reverse_lazy('users:password_reset_done')
         ),
         name='password_reset_form'
     ),
@@ -40,7 +40,7 @@ urlpatterns = [
         'password_change/',
         PasswordChangeView.as_view(
         template_name='users/password_change_form.html',
-        success_url = reverse_lazy('users:password_change_done')
+        success_url=reverse_lazy('users:password_change_done')
         ),
         name='password_change_form'
     ),
